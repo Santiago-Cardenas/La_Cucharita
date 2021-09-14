0bf59f0bec7592912c0e95f322a90aa07e1cf7ca
Santiago <santiagocardenasmontes@gmail.com>
2021-09-14 14:24:45 -0500

    Version 1.3

---
+++ src/model/Ingredients.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Ingredients {
+
+	private String ingredientName;
+
+	public Ingredients(String ingredientName) {
+		this.ingredientName = ingredientName;
+	}
+
+	public String getIngredientName() {
+		return ingredientName;
+	}
+
+	public void setIngredientName(String ingredientName) {
+		this.ingredientName = ingredientName;
+	}
+	
+	
+}
--- src/ui/CucharitaGUI.java
+++ src/ui/CucharitaGUI.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import model.User;
