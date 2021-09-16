1e85e5f1f0c5d5dca6e7f18c17f5aa96ba61d257
Santiago <santiagocardenasmontes@gmail.com>
2021-09-16 17:49:29 -0500

    Version 1.4 replaced Inventory structure

--- src/model/Ingredients.java
+++ src/model/Ingredients.java
@@ -3,18 +3,35 @@ package model;
 public class Ingredients {
 
 	private String ingredientName;
-
-	public Ingredients(String ingredientName) {
+	private int ingredientQT;
+	private String ingredientUnits;
+	
+	protected Ingredients(String ingredientName, int ingredientQT, String ingredientUnits) {
 		this.ingredientName = ingredientName;
+		this.ingredientQT = ingredientQT;
+		this.ingredientUnits = ingredientUnits;
 	}
-
 	public String getIngredientName() {
 		return ingredientName;
 	}
-
 	public void setIngredientName(String ingredientName) {
 		this.ingredientName = ingredientName;
 	}
+	public int getIngredientQT() {
+		return ingredientQT;
+	}
+	public void setIngredientQT(int ingredientQT) {
+		this.ingredientQT = ingredientQT;
+	}
+	public String getIngredientUnits() {
+		return ingredientUnits;
+	}
+	public void setIngredientUnits(String ingredientUnits) {
+		this.ingredientUnits = ingredientUnits;
+	}
+
+	
+	
 	
 	
 }
